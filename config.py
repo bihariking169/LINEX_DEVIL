14af36402049c810381d9a2f6cdec91ec164e406
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-09-18 14:17:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/bihariking169/LINEX_
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BRANDED_LINK")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/black_devil_group")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/BRANDRD_21")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
