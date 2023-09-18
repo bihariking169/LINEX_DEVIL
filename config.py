bafd6c1e9a5af5bab27825a34f339eb228dd441a
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-09-18 14:15:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Siddhi ダ ᴍᴜsɪᴄ")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "linex_devil_music_bot")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "6258877205").split()))
 
