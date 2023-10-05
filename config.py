7bcc1f800223af95760b8f3ad967def99fe8df3e
ALONE HAPPY <144148196+bihariking169@users.noreply.github.com>
2023-10-06 02:45:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/bihariking169/LINEX_
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/bokt_fucker_chnl")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/The_Fllirting_Zone")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/lll_ITZ_ME_lll")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/STRENGER_CLUB")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
